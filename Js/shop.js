73390ab753d51c5fb3600e42743fcae6a1431838
AdilJabbr <adilshj@code.edu.az>
2025-06-09 01:27:52 +0400

    shopnew

---
+++ Js/shop.js
@@ -0,0 +1,20 @@
+const accordions = document.querySelectorAll('.accordion');
+accordions.forEach(btn => {
+  btn.addEventListener('click', function () {
+    this.classList.toggle('active');
+    const panel = this.nextElementSibling;
+    panel.classList.toggle('show');
+  });
+});
+
+const shopsubmenu =document.querySelector(".shop-submenu");
+const shopsubmenubtn=document.getElementById("shopsubmenubtn")
+const closeshopsubmenubtn=document.getElementById("closeshopsubmenubtn")
+shopsubmenu.classList.add("hidden");
+shopsubmenubtn.addEventListener("click", () => {
+  shopsubmenu.classList.remove("hidden");
+
+});
+closeshopsubmenubtn.addEventListener("click", () => {
+  shopsubmenu.classList.add("hidden");
+});
