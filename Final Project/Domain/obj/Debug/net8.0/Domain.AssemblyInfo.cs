cd9c8cada80123c9bc20c295d53a625be563d540
AdilJabbr <adilshj@code.edu.az>
2025-06-29 23:12:40 +0400

    commit

--- Final Project/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Final Project/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52f0e0845f32f060735579ef9d48531f2b59ed2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+860eab663be3befa436735e1a3256453113caeea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
